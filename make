eb69ee5a0eca5852cfb9cf8adab82f60fd402a63
morganllewellynjones <morgan@debianhome.hsd1.or.comcast.net>
2024-05-05 16:27:43 -0700

    The container now sets a new hostname and UTS, new PID namespace, new mount namespace and new network namespace. No work yet done with control groups. User namespace needs to wait until I figure out how to run the container as a non-root user.

---
+++ make
@@ -0,0 +1,2 @@
+cargo build
+sudo ./target/debug/rust-container /usr/bin/bash
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
-use std::{env, process, thread, time};
-use std::os::unix::fs;
-// use std::io;
+use errno::errno;
+use libc::*;
+use std::env;
+use std::ffi::{CString, OsStr, OsString};
+use std::process::{Command, Stdio};
+use std::{thread, time};
 
-fn main() -> std::io::Result<()> {
+fn c_error_check(status: c_int, fn_name: &str) {
+    if status != 0 {
+        panic!("{fn_name}: {}", errno().to_string());
+    }
+}
 
-//    let args: Vec<String> = env::args().collect();
-//    dbg!(args);
+fn run() {
+    let mut args: Vec<String> = env::args().collect();
 
-    fs::chroot("debian_container")?;
-    env::set_current_dir("debian_container")?;
+    let newroot = Command::new("chroot")
+        .arg("debian_container")
+        .args(args.drain(1..))
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap();
+}
 
-    println!("pid is {}", process::id());
+fn jail() {
 
-    let waittime = time::Duration::from_secs(3000);
-    thread::sleep(waittime);
-    Ok(())
+    let newhostname = CString::new("debiancontainer").unwrap();
+    let newhostnameptr = newhostname.as_ptr();
+    unsafe {
+        _ = c_error_check(
+            unshare(
+                CLONE_NEWPID
+                    | CLONE_NEWNET
+                    | CLONE_NEWNS
+                    | CLONE_NEWPID
+                    | CLONE_NEWUTS
+            ),
+            "unshare",
+        );
+        _ = c_error_check(sethostname(newhostnameptr, 15), "sethostname");
+    }
+
+    let mut status = 0;
+    unsafe {
+        match fork() {
+            -1 => panic!("fork error"),
+            0 => {
+                run();
+            }
+            _ => {
+                wait(&mut status as *mut c_int);
+            }
+        }
+    };
+}
+
+fn main() {
+    jail();
 }
