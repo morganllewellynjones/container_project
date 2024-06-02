ee1ccdd5903faa736daa5db664813d05a94fb3e9
morganllewellynjones <morganjones.gm@gmail.com>
2024-06-02 15:09:55 -0700

    Transfered code to use Rust standard library features instead of C interop

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,175 @@
-use errno::errno;
-use getopt::Opt;
-use libc::*;
 use std::env;
-use std::ffi::CString;
-use std::process::{Command, Stdio};
+use std::process::{self, Stdio};
 
-#[derive(Debug)]
-struct Config {
-    cpus_limit: u8,
-    memory_limit: u64,
-    pids_limit: u64,
-    hostname: CString,
-    username: CString,
-    directory: String,
-    ports_published: Vec<Port>,
-    cmd: String,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            cpus_limit: 0,
-            memory_limit: 0,
-            pids_limit: 0,
-            hostname: CString::new("container").unwrap(),
-            username: CString::new("").unwrap(),
-            directory: "debian_container".to_string(),
-            ports_published: vec![],
-            cmd: "/usr/bin/bash".to_string(),
-        }
-    }
-}
-
-#[derive(Debug, Default)]
-struct Port {
-    host: u16,
-    dest: u16,
-}
-
-#[derive(Debug, Default)]
-struct BindMount {
-    source: String,
-    destination: String,
-}
-
-fn c_error_check(status: c_int, fn_name: &str) {
-    if status != 0 {
-        panic!("{fn_name}: {}", errno().to_string());
-    }
-}
-
-fn jail(config: &Config) {
-    let newhostname = &config.hostname;
-    let newhostnameptr = newhostname.as_ptr();
-    unsafe {
-        _ = c_error_check(
-            unshare(CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUTS),
-            "unshare",
-        );
-        _ = c_error_check(sethostname(newhostnameptr, 15), "sethostname");
-    }
-}
-
-fn run(config: Config) {
-
-    jail(&config);
-
-    let newroot = Command::new("chroot")
-        .arg(&config.directory)
-        .arg(&config.cmd)
+fn setup_container() -> process::Child {
+    let child = process::Command::new("unshare")
+        .args([
+            "--pid",
+            "--mount",
+            "--net",
+            "--uts",
+            "--cgroup",
+            "--time",
+            "--mount-proc",
+            "--kill-child",
+            "--root=debian_container",
+            "--wd=debian_container",
+        ])
+        .args(["/bin/bash"])
         .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to create unshared command.");
+
+    let ns = child.id().to_string();
+
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "proc",
+            "--source",
+            "/proc",
+            "--target",
+            "debian_container/proc",
+            "--options",
+            "noexec,nosuid,nodev",
+        ])
         .output()
-        .unwrap();
+        .expect("Failed mounting file");
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "tmpfs",
+            "--source",
+            "/dev",
+            "--target",
+            "debian_container/dev",
+            "--options",
+            "noexec,strictatime",
+        ])
+        .output()
+        .expect("Failed mounting file");
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "tmpfs",
+            "--source",
+            "/dev/shm",
+            "--target",
+            "debian_container/dev/shm",
+            "--options",
+            "noexec,nosuid,nodev",
+        ])
+        .output()
+        .expect("Failed mounting file");
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "mqueue",
+            "--source",
+            "/dev/mqueue",
+            "--target",
+            "debian_container/dev/mqueue",
+            "--options",
+            "noexec,nosuid,nodev",
+        ])
+        .output()
+        .expect("Failed mounting file");
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "devpts",
+            "--source",
+            "/dev/pts",
+            "--target",
+            "debian_container/dev/pts",
+            "--options",
+            "noexec,nosuid",
+        ])
+        .output()
+        .expect("Failed mounting file");
+    process::Command::new("mount")
+        .args([
+            "--namespace",
+            &ns,
+            "--types",
+            "sysfs",
+            "--source",
+            "/sys",
+            "--target",
+            "debian_container/sys",
+            "--options",
+            "noexec,nosuid,nodev,rdonly",
+        ])
+        .output()
+        .expect("Failed mounting file");
+
+    process::Command::new("chmod")
+        .args(["755", "debian_container/dev"])
+        .output()
+        .expect("Failed changing file permissions");
+    process::Command::new("chmod")
+        .args(["1777", "debian_container/dev/shm"])
+        .output()
+        .expect("Failed changing file permissions");
+    process::Command::new("chmod")
+        .args(["620", "debian_container/dev/pts"])
+        .output()
+        .expect("Failed changing file permissions");
+
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/null"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/zero"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/full"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/tty"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/random"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mkdir")
+        .args(["0666", "debian_container/dev/urandom"])
+        .output()
+        .expect("Failed making debian_container/dev subdirectory");
+    process::Command::new("mount")
+        .args(["/etc/", "debian_container/etc"])
+        .output()
+        .expect("Failed to mount /etc folder");
+
+    process::Command::new("ln")
+        .args(["-s", "/dev/ptmx", "debian_container/dev/ptmx"])
+        .output()
+        .expect("Failed to symlink /dev/ptmx");
+
+    return child;
+
 }
 
 fn main() {
-    let mut config: Config = Default::default();
-    let mut args: Vec<String> = env::args().collect();
-    let mut opts = getopt::Parser::new(&args, "h:u:d:");
-    let usage = "rc [-u username] [-h hostname] [-d container_directory] command".to_string();
-
-    loop {
-        match opts.next().transpose().unwrap() {
-            None => break,
-            Some(opt) => match opt {
-                Opt('h', Some(arg)) => config.hostname = CString::new(arg.clone()).unwrap(),
-                Opt('u', Some(arg)) => config.username = CString::new(arg.clone()).unwrap(),
-                Opt('d', Some(arg)) => config.directory = arg.clone(),
-                _ => {
-                    println!("{usage}");
-                    return;
-                }
-            },
-        }
-    }
-    dbg!(&config);
-    run(config);
+   
+    let child = setup_container();
+    child
+        .wait_with_output()
+        .expect("Failed to execute unshared command.");
 }
